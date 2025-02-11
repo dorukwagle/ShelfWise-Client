dbf723e3a1fd894c251b18f22db9fc88d7b48372
dorukwagle <dorukwagle@gmail.com>
2025-02-11 05:52:55 +0545

    fix cache key duplication

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -4,7 +4,7 @@ import { AxiosError } from "axios";
 import { ErrorRes } from "../entities/ErrorRes";
 import Genre from "../entities/Genre";
 import genreService from "../services/genreService";
-import { GENRES_CACHE_KEY, USER_CACHE_KEY } from "../entities/constants";
+import { GENRES_CACHE_KEY } from "../entities/constants";
 
 const useGenre = (onSuccess?: () => void) => {
     const queryClient = useQueryClient();
