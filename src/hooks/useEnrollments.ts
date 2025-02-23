2326ed1802f78658f368c9af8096ad13bbd94d68
Aashika101 <raishallona@gmail.com>
2025-02-23 22:50:51 +0545

    Search filter enrollments fixed

--- src/hooks/useEnrollments.ts
+++ src/hooks/useEnrollments.ts
@@ -1,25 +1,23 @@
 // src/hooks/useEnrollments.ts
 import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import Enrollment from "../entities/enrollements";
-import { ENROLL_CACHE_KEY } from "../entities/constants";
+import { ENROLL_CACHE_KEY, DEFAULT_PAGE_SIZE } from "../entities/constants"; 
 import enrollmentFetchService from "../services/enrollmentFetchService";
+import Enrollment from "../entities/enrollements";
 
-const DEFAULT_PAGE_SIZE = 15;
-
-const fetchEnrollments = async ({ page, pageSize }: {page: number; pageSize: number}) => {
+const fetchEnrollments = async ({ page, pageSize }: { page: number; pageSize: number }) => {
     const response = await enrollmentFetchService.get(`?page=${page}&pageSize=${pageSize}`);
     return response;
 };
 
-const useEnrollments = (page:number, pageSize:number) => {
-  return useQuery<Enrollment[], AxiosError>({
-    queryKey: [ENROLL_CACHE_KEY, page, pageSize],
-    queryFn: () => fetchEnrollments({ page, pageSize }),
-  });
+const useEnrollments = (page: number, pageSize: number) => {
+    return useQuery<Enrollment[], AxiosError>({
+        queryKey: [...ENROLL_CACHE_KEY, page, pageSize], 
+        queryFn: () => fetchEnrollments({ page, pageSize }),
+    });
 };
 
-export { DEFAULT_PAGE_SIZE };
 export default useEnrollments;
+
 
 
